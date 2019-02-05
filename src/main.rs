b900bbe3b8886570ce07c0026feef38f1f2be73d
Agent47 <654136828@qq.com>
2019-02-05 10:48:24 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::collections::HashMap;
+
+
 fn main() {
 //    ch08_01_vectors();
-    ch08_02_strings()
+//    ch08_02_strings();
+    ch08_03_hash_maps();
 }
 
 fn ch08_01_vectors() {
@@ -125,6 +129,154 @@ fn ch08_02_strings(){
         // 可以按照个人喜好选择使用
     }
 
+    // 更新字符串
+    {
+        // 使用push 换个 push_str 更新字符串
+        // 可以通过 push_str 方法来附加**字符串 slice**，从而使 String 变长
+        let mut s1 = String::from("foo");
+        let s2 = "bar";
+        s1.push_str(s2);
+        println!("s2 is {}", s2);
+
+        // push 方法被定义为获取一个单独的字符作为参数，并附加到 String 中。
+        let mut s3 = String::from("lo");
+        s3.push('l');
+        println!("s3 is {}", s3);
+    }
+
+    // 使用 + 运算符 或者 format!宏拼接字符串
+    {
+
+        let s1 = String::from("Hello, ");
+        let s2 = String::from("world");
+        let s4 = String::from("!");
+        // &s2 是 &String 可以被 强转（coerced）成 &str。
+        // Rust 使用了一个被称为 解引用强制多态（deref coercion）的技术，你可以将其理解为它把 &s2 变成了 &s2[..]。
+        let s3 = s1 + &s2 + &s4; // 注意 s1 被移动了，不能继续使用
+//        println!("{}",s1);
+        println!("{}",s2);
+        println!("{}",s3);
+
+        let s1 = String::from("tic");
+        let s2 = String::from("tac");
+        let s3 = String::from("toe");
+
+        let s = format!("{}-{}-{}", s1, s2, s3);
+        println!("{}",s)
+    }
+    // 字符串 slice
+    {
+        let hello = "Здравствуйте";
+
+        let s = &hello[0..4]; // 每个 Unicode 标量值需要两个字节存储
+
+        println!("{}",s)
+    }
+    // 遍历字符串
+    {
+        // 返回六个 char 类型的值
+        for c in "नमस्ते".chars() {
+            println!("{}", c);
+        }
+
+        // bytes 方法返回每一个原始字节
+        for b in "नमस्ते".bytes() {
+            println!("{}", b);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+fn ch08_03_hash_maps(){
+    //  哈希 map（hash map）。HashMap<K, V> 类型储存了一个键类型 K 对应一个值类型 V 的映射。
+    // 它通过一个 哈希函数（hashing function）来实现映射，决定如何将键和值放入内存中。
+    // 很多编程语言支持这种数据结构，不过通常有不同的名字：哈希、map、对象、哈希表或者关联数组，
+
+    // 新建一个 hash map
+    {
+        use std::collections::HashMap;
+
+        let mut scores = HashMap::new();
+
+        scores.insert(String::from("Blue"), 10);
+        scores.insert(String::from("Yellow"), 50);
+
+        // 使用两个vector 生成 hashMap
+
+        let teams  = vec![String::from("Blue"), String::from("Yellow")];
+        let initial_scores = vec![10, 50];
+
+        let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    }
+    // 哈希 map 和所有权
+    {
+
+        let field_name = String::from("Favorite color");
+        let field_value = String::from("Blue");
+
+        let mut map = HashMap::new();
+        map.insert(field_name, field_value);  // 移交所有权 虽然也能借用但是并不常用
+
+        println!("{:?}",map);
+
+    }
+
+    // 访问哈希 map 中的值
+    {
+
+        let mut scores = HashMap::new();
+
+        scores.insert(String::from("Blue"), 10);
+        scores.insert(String::from("Yellow"), 50);
+
+        let team_name = String::from("Blue");
+        let score = scores.get(&team_name); // 返回的是 Option<&T> 类型
+    }
+
+    {
+        let mut scores = HashMap::new();
+
+        scores.insert(String::from("Blue"), 10);
+        scores.insert(String::from("Yellow"), 50);
+
+        for (key, value) in &scores {
+            println!("{}: {}", key, value);
+        }
+    }
+
+    // 更新哈希 map
+    // 覆盖一个值
+    {
+        let mut scores = HashMap::new();
+        scores.insert(String::from("Blue"), 10);
+        scores.insert(String::from("Blue"), 25);
+        println!("{:?}", &scores);
+    }
+    // 只在键没有对应值时插入
+    {
+        // 此哈希 map 有一个特有的 API，叫做 entry，它获取我们想要检查的键作为参数
+        // entry 函数的返回值是一个枚举
+        // Entry，它代表了可能存在也可能不存在的值
+        let mut scores = HashMap::new();
+        scores.insert(String::from("blue"),10);
+
+        // 使用 entry 方法只在键没有对应一个值时插入
+        scores.entry(String::from("Yellow")).or_insert(50);
+        scores.entry(String::from("Blue")).or_insert(50);
+
+        println!("{:?}", scores);
+    }
 
 
 
