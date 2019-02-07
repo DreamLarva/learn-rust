2d5d52fc623f1d734519adc77224093224238a5c
Agent47 <654136828@qq.com>
2019-02-07 15:02:42 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,20 @@ use std::io::Read;
 
 mod ch08;
 mod ch09;
+mod ch10;
 
 fn main() {
-    ch08::ch08_01_vectors();
-    ch08::ch08_02_strings();
-    ch08::ch08_03_hash_maps();
+//    ch08::ch08_01_vectors();
+//    ch08::ch08_02_strings();
+//    ch08::ch08_03_hash_maps();
 
-    ch09::ch09_01_unrecoverable_errors_with_panic();
-    ch09::ch09_02_recoverable_errors_with_result();
-    ch09::ch09_03_to_panic_or_not_to_panic();
+//    ch09::ch09_01_unrecoverable_errors_with_panic();
+//    ch09::ch09_02_recoverable_errors_with_result();
+//    ch09::ch09_03_to_panic_or_not_to_panic();
+
+    ch10::ch10_01_syntax();
+
+
 }
 
 
