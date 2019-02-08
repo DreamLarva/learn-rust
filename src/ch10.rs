ac7baaa2ec6f68963b806a3039e1871bf5a146a7
Agent47 <654136828@qq.com>
2019-02-08 09:54:03 +0800

    1

--- src/ch10.rs
+++ src/ch10.rs
@@ -17,19 +17,42 @@ pub fn ch10_01_syntax() {
         let both_integer = Point { x: 5, y: 10 };
         let both_float = Point { x: 1.0, y: 4.0 };
         let integer_and_float = Point { x: 5, y: 4.0 };
-
     }
     // 枚举定义中的泛型
     {
-        enum Option<T>{
+        enum Option<T> {
             Some(T),
             None,
         }
-        enum Result<T,E> {
+        enum Result<T, E> {
             Ok(T),
             Err(E),
         }
     }
+    // 方法中使用泛型
+    {
+        struct Point<T> {
+            x: T,
+            y: T,
+        }
+        // 注意必须在 impl 后面声明 T,，这样就可以在 Point<T> 上实现的方法中使用它了
+        impl<T> Point<T> {
+            fn x(&self) -> &T {
+                &self.x
+            }
+        }
+        // 给予是 f32 类型才有的方法(有点像重载)
+        impl Point<f32> {
+            fn distance_from_origin(&self) -> f32 {
+                (self.x.powi(2) + self.y.powi(2)).sqrt()
+            }
+        }
 
+        let p = Point { x: 5, y: 10 };
+        let q = Point { x: 5.0, y: 10.0 };
 
+        println!("p.x = {}", p.x());
+//        println!("p.x = {}", p.distance_from_origin()); // 报错
+        println!("p.x = {}", q.distance_from_origin());
+    }
 }
